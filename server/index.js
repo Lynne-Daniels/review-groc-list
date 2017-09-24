5d83a67a4d51f3cdd01ed9cced69b45ae1a9b842
Shi-Hao Hong <shihaohong94@gmail.com>
2017-09-24 14:58:35 -0700

    included server-side routes

--- server/index.js
+++ server/index.js
@@ -1,7 +1,30 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+var groceryList = [
+  {id: 1, quantity: 5, description: "frozen pizza"},
+  {id: 2, quantity: 10, description: "greek yogurt"},
+  {id: 3, quantity: 2, description: "wine"},
+  {id: 4, quantity: 1, description: "iced coffee"}
+]
 
 app.use(express.static(__dirname + '/../client/dist'));
+
+app.use(bodyParser.json());
+
+//GET route
+app.get('/groceries', function(req, res) {
+  res.status(200);
+  res.send(groceryList);
+})
+//POST route
+app.post('/groceries', function(req, res) {
+  console.log('req.body', req.body);
+  groceryList.push(req.body);
+  res.status(201);
+  res.end('Information was received');
+})
 
 app.listen(3000, function() {
   console.log('Server started and listening on port 3000');
