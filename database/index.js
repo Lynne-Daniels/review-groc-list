68cc1443f3fa090689a2d939c84b092a977e2fc6
Lynne Daniels <lynne33477@gmail.com>
2017-09-24 18:42:29 -0700

    Update index.js

--- database/index.js
+++ database/index.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',  //@TODO: Change user to your local mysql instance username
-  password : 'plantlife',   //@TODO: Change password to your local mysql instance password
+  password : 'see slack',   //@TODO: Change password to your local mysql instance password
   database : 'review'    //@TODO: Ensure database name matches the database you created
 });
 
